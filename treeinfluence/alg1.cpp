03c7d7a182726a87e3e3600cdba9a9fffced9b4b
“JoelJosephPaul” <joelkanjirathinkal@gmail.com>
2021-02-01 19:38:19 +0530

    tree influence done

---
+++ treeinfluence/alg1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <map>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	string buffer;
+	ifstream fp;
+	fp.open("ipdata.txt");
+	// 	, ( ) eof
+	int l = 1, n = 0;
+	char c;
+	map<string, float> influence;
+	int mval = 0;
+	while (!fp.eof())
+	{
+		fp >> c;
+		switch (c)
+		{
+		case ',':
+			if (buffer.size())
+				influence[buffer] = l;
+			buffer = "";
+			break;
+		case '(':
+			if (buffer.size())
+				influence[buffer] = l;
+			buffer = "";
+			l++;
+			break;
+		case ')':
+			if (buffer.size())
+				influence[buffer] = l;
+			buffer = "";
+			l--;
+			break;
+		default:
+			buffer += c;
+		}
+		if (l > mval)
+			mval = l;
+	}
+	fp.close();
+	float var = 1.0 / mval;
+	cout << "var " << var << endl;
+	for (auto &i : influence)
+		i.second = 1 - (i.second - 1) * var;
+	for (auto i : influence)
+	{
+		cout << i.first << " : " << i.second << endl;
+	}
+	return 0;
+}
